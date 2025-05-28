02e447c7cf295c3136982d55bcaf664d235b141d
Malik-p <sahilmaik2005@gmail.com>
2025-05-28 22:40:57 +0530

    .

--- server/routes/hotelRoutes.js
+++ server/routes/hotelRoutes.js
@@ -1,5 +1,6 @@
 import express from "express"
 import { registerHotel } from "../controller/hotelController.js";
+import hotelController from '../controller/hotelController.js';
 
 const hotelRouter = express.Router();
 
