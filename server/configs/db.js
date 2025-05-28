ee8d0bb0e594069716f61c38a16c38c0ae4ed673
Malik-p <sahilmaik2005@gmail.com>
2025-05-28 16:56:24 +0530

    changes

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 
-const connectDB = async ()=>{
-    try{
-        mongoose.connection.on('connected',()=>console.log("Database is connected"))
-        await mongoose.connect(`${process.env.MONGODB_URI/hotelBooking}`)
-    } catch(error){
-          console.log(error.message);     
+const connectDB = async () => {
+    try {
+        mongoose.connection.on('connected', () => console.log("Database is connected"));
+
+        await mongoose.connect(`${process.env.MONGODB_URI}/hotelBooking`);
+    } catch (error) {
+        console.log(error.message);
     }
-}
+};
 
 export default connectDB;
+
 
